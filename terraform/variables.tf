cb51da513d8a75bf2fcdd094011afa5967290ae8
Joe K <jkrilov@gmail.com>
2023-02-16 17:04:57 -0500

    fix: fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "projectName" {
-  type        = string
+  type      = string
   description = "Provide a project name. This will be used for the base Resource Group and State File name."
 }
